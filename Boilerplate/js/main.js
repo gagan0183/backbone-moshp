7a9e74c9926ece2ce231c4b446ea69f2d3587502
Gagan <g.deepsingh1@gmail.com>
2018-07-21 00:42:57 +0530

    setting attribute and validation

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -6,8 +6,20 @@
 var Song = Backbone.Model.extend({
     initialize: function() {
         console.log("A song create");
+    },
+    validate: function(attrs) {
+        if(!attrs.p) {
+            return  "This value is not there";
+        }
     }
 });
 
 var song = new Song();
 console.log(song);
+
+console.log('Before setting attribute : ', song.isValid());
+console.log('Before setting attribute : ', song.validationError);
+
+song.set('p', 'p');
+console.log('After setting attribute : ', song.isValid());
+console.log('After setting attribute : ', song.validationError);
